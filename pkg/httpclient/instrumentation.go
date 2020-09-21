7b7dce88959189259ce0d0bc7b5331afc933a072
David Lienardy <david.lienardy@payfazz.com>
2020-09-21 17:12:04 +0700

    add path pattern matching, and fix roundtripper on cb

--- pkg/httpclient/instrumentation.go
+++ pkg/httpclient/instrumentation.go
@@ -2,7 +2,9 @@ package httpclient
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+	"regexp"
 	"time"
 
 	"github.com/prometheus/client_golang/prometheus"
@@ -24,8 +26,31 @@ func requestDuration(histogram *prometheus.HistogramVec, name, scheme, host, pat
 	).Observe(duration.Seconds())
 }
 
+func pathPatternMatching(pathPatterns map[string]string, cmpPath string) string {
+	for k, v := range pathPatterns {
+		match, err := regexp.MatchString(v, cmpPath)
+		if err != nil {
+			log.SetFlags(log.Lshortfile)
+			log.Println(err)
+
+			// just return path what want to be match
+			return cmpPath
+		}
+
+		// return path pattterned
+		// example:
+		// - /users/:id
+		// - /users/{id}
+		if match {
+			return k
+		}
+	}
+
+	return cmpPath
+}
+
 // NewInstrumentation .
-func NewInstrumentation(histogram *prometheus.HistogramVec, name string, c *http.Client) *http.Client {
+func NewInstrumentation(hv *prometheus.HistogramVec, name string, pathPatterns map[string]string, c *http.Client) *http.Client {
 	std := &http.Client{Timeout: 30 * time.Second}
 	transport := http.DefaultTransport
 	if c != nil {
@@ -36,13 +61,13 @@ func NewInstrumentation(histogram *prometheus.HistogramVec, name string, c *http
 		}
 	}
 
-	std.Transport = instrumentRoundTripper(histogram, name, transport)
+	std.Transport = instrumentRoundTripper(hv, name, pathPatterns, transport)
 
 	return c
 }
 
 // NewWithDefaultInstrumentation .
-func NewWithDefaultInstrumentation(name string, c *http.Client) *http.Client {
+func NewWithDefaultInstrumentation(name string, pathPatterns map[string]string, c *http.Client) *http.Client {
 	std := &http.Client{Timeout: 30 * time.Second}
 	transport := http.DefaultTransport
 	if c != nil {
@@ -53,23 +78,26 @@ func NewWithDefaultInstrumentation(name string, c *http.Client) *http.Client {
 		}
 	}
 
-	var metric = promauto.NewHistogramVec(prometheus.HistogramOpts{
+	var hv = promauto.NewHistogramVec(prometheus.HistogramOpts{
 		Name:    "outgoing_http_request_duration_seconds",
 		Help:    "observe elapsed time in seconds for a outgoing request",
 		Buckets: []float64{0.5, 1, 15, 30, 60},
 	}, []string{"name", "scheme", "host", "path", "method", "code"})
 
-	std.Transport = instrumentRoundTripper(metric, name, transport)
+	std.Transport = instrumentRoundTripper(hv, name, pathPatterns, transport)
 
 	return c
 }
 
-func instrumentRoundTripper(histogram *prometheus.HistogramVec, name string, next http.RoundTripper) RoundTripperFunc {
+func instrumentRoundTripper(hv *prometheus.HistogramVec, name string, pathPatterns map[string]string, next http.RoundTripper) RoundTripperFunc {
 	return RoundTripperFunc(func(r *http.Request) (*http.Response, error) {
 		now := time.Now()
 		res, err := next.RoundTrip(r)
 		if err == nil {
-			requestDuration(histogram, name, r.URL.Scheme, r.URL.Host, r.URL.Path, r.Method, res.StatusCode, now)
+			defer func() {
+				path := pathPatternMatching(pathPatterns, r.URL.Path)
+				requestDuration(hv, name, r.URL.Scheme, r.URL.Host, path, r.Method, res.StatusCode, now)
+			}()
 		}
 
 		return res, err
